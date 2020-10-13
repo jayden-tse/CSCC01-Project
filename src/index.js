43afae13f8ec5b818bdcdf2476949530b7359ae9
Jayden <j_aydentse@hotmail.com>
2020-10-13 01:24:50 -0400

    Added server into index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ const MongoClient = require('mongodb').MongoClient;
 const URI = "mongodb+srv://SPORTCRED:1234@sportcred.q4w2z.mongodb.net/SPORTCRED?retryWrites=true&w=majority";
 const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const express = require('express');
+const app = express();
+const port = 8080;
+
 async function main() {
     try {
         await client.connect();
+        app.get('/', (req, res) => {
+            console.log("Connected!");
+            res.send('Hello World!')
+        });
+        app.listen(port, () => {
+            console.log(`Example app listening at http://localhost:${port}`)
+        });
     } catch (e) {
         console.error(e);
     } finally {
