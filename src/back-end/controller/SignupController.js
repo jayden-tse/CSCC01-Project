50c39f05d961bb2664b4f6ad8759b0958712a812
{Micki7} <{mickiasmarcos@gmail.com}>
2020-10-14 16:57:04 -0400

    Got rid of Notify.js and put the code into SignUpController class

--- src/back-end/controller/SignupController.js
+++ src/back-end/controller/SignupController.js
@@ -1,3 +1,34 @@
 class SignupController {
-    
+    nodemailer = require('nodemailer');
+    // const MongoClient = require('mongodb').MongoClient;
+    // const URI = "mongodb+srv://SPORTCRED:1234@sportcred.q4w2z.mongodb.net/SPORTCRED?retryWrites=true&w=majority";
+    // const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    // Business email from which users will get the confirmation.
+    transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'sportcredyes@gmail.com',
+            pass: 'projectyes'
+            }
+        });
+
+
+    notifyUserForNewAccount(user) {
+        var mailOptions = {
+            from: 'sportcredyes@gmail.com',
+            to: user.email,
+            subject: 'New Account',
+            text: 'Your SportCred account has been created successfully.'
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    }
+
 }
