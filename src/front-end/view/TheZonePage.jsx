9446c7a371055715ac08ae551cfd89c0a10df48e
yf10945 <71033521+yf10945@users.noreply.github.com>
2020-10-20 10:15:35 -0400

    YES-101 fixed event handlers for top nav bar

---
+++ src/front-end/view/TheZonePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import TheZone from '../components/theZone/TheZone';
+import TopNavBar from '../components/general/TopNavBar';
+// replace whatever is in render with components
+// needed for the page
+
+class TheZonePage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div>
+        <TopNavBar handleLogout={this.props.handleLogout}
+          redirectToProfile={this.props.redirectToProfile}/>
+        <TheZone redirectToDebate={this.props.redirectToDebate}
+          redirectToOpenCourt={this.props.redirectToOpenCourt}
+          redirectToPicksAndPredictions={this.props.redirectToPicksAndPredictions}
+          redirectToTrivia={this.props.redirectToTrivia}/>
+      </div>
+    )
+  }
+}
+
+export default TheZonePage;
