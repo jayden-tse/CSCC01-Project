58fbcf4f8cd10d89c6c9bbb2422f0b24f3d00ca4
Howard Hong <howard.hong@mail.utoronto.ca>
2020-10-28 18:37:36 -0400

    Changed login to a PUT request and renamed auth_get to auth and auth_put to deauth.

--- src/routes.js
+++ src/routes.js
@@ -35,10 +35,10 @@ router.delete('/user', signupController.user_del);
 /* LOGIN */
 
 // Get login authentication
-router.get('/login', loginController.auth_get);
+router.put('/login', loginController.auth);
 
 // Logout
-router.put('/logout', loginController.auth_put);
+router.put('/logout', loginController.deauth);
 
 
 /* PROFILE */
