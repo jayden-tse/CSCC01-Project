505b47f3e7a5b20ca128b33b3703d2be50d78fe6
Phillip Pham <phoneqw@gmail.com>
2020-10-28 18:38:34 -0400

    YES-95 changed login to put request

--- front-end/src/components/login/LoginCalls.js
+++ front-end/src/components/login/LoginCalls.js
@@ -1,13 +1,19 @@
 var startUrl = "http://localhost:8080/login";
 
 export async function login(username, password) {
-  var newUrl = new URL(startUrl);
+  //var newUrl = new URL(startUrl);
   var params = { username: username, password: password };
-  newUrl.search = new URLSearchParams(params).toString();
-  console.log(newUrl);
-  return fetch(newUrl, {
+  //newUrl.search = new URLSearchParams(params).toString();
+  var fetchOptions = {
+    method: "PUT",
     mode: "cors",
-  })
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  };
+  //console.log(fetchOptions);
+  return fetch(startUrl, fetchOptions)
     .then((res) => {
       console.log("response: " + res.body.text);
       return res;
