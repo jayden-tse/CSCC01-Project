a0ec5b984b507d54d83aa34d63cf8e3d45bf7746
Howard Hong <howard.hong@mail.utoronto.ca>
2020-11-09 16:09:16 -0500

    [YES-38] Implemented voting (accessed by /thezone/post/update/vote with params vote (-1/1) and postid), not bug free yet.

--- src/back-end/controller/TheZoneController.js
+++ src/back-end/controller/TheZoneController.js
@@ -9,7 +9,7 @@ exports.the_zone_post_put = async function(req, res) {
         // user authenticated
         // user, date, content, agree, disagree, comments
         try {
-            await dbCreate.createPost(req.session.passport.user, new Date(), req.body.content, 0, 0, [], []);
+            await dbCreate.createPost(req.session.passport.user, new Date(), req.body.content, 0, 0, [], [], []);
             res.sendStatus(200);
         } catch (e) {
             console.log(e);
@@ -50,14 +50,23 @@ exports.the_zone_update_post_put = function(req, res) {
     res.send('NOT IMPLEMENTED');
 };
 
-exports.the_zone_update_agree_put = function(req, res) {
+exports.the_zone_update_vote_put = async function(req, res) {
     if (req.user) {
         try {
             // update likes here
-
-        } catch {
-
+            let username = { 'username': req.session.passport.user };
+            let result = await dbUpdate.updateVote(username, req.body.vote, req.body.postid);
+            if (result && result.modifiedCount > 0) {
+                res.sendStatus(200);
+            } else {
+                res.status(400).send('BAD INPUT');
+            }
+        } catch (e) {
+            console.log(e);
+            res.status(500).send('WRITE FAILED');
         }
+    } else {
+        res.status(401).send('NOT AUTHORIZED');
     }
 };
 
--- src/back-end/model/DatabaseUpdate.js
+++ src/back-end/model/DatabaseUpdate.js
@@ -56,19 +56,36 @@ class DatabaseUpdate {
         return result;
     }
 
-    async updateVote(req, postid) {
-        let username = { 'username': req.session.passport.user };
+    async updateVote(username, vote, postid) {
         console.log(username);
         let post = { '_id': ObjectId(postid) };
         let postDoc = await mongoConnect.getDBCollection("Posts").findOne(post);
+        console.log(postDoc);
         if (postDoc !== null) {
-            let agreed = postDoc.agreeusers;
-            let disagreed = postDoc.disagreed;
-            if (agreed.find(username)) {
-                // user already liked it, set it back to neutral
-            }
-        }
-        return result;
+            let agreed = postDoc.usersagreed; // should be an array of usernames in json
+            let disagreed = postDoc.usersdisagreed;
+            if (vote > 0) {
+                if (agreed.indexOf(username) !== -1) {
+                    agreed = agreed.filter(function(e) {
+                        return e !== agreed.find(username);
+                    });
+                } else {
+                    agreed.splice(agreed.length, 0, username);
+                }
+                console.log("agreed " + agreed);
+                return this.updatePost(postid, "usersagreed", agreed); // either way update usersagreed on the post
+            } else if (vote < 0) {
+                if (disagreed.indexOf(username) !== -1) {
+                    disagreed = disagreed.filter(function(e) {
+                        return e !== disagreed.find(username);
+                    });
+                } else {
+                    disagreed.splice(disagreed.length, 0, username);
+                }
+                console.log("disagreed " + disagreed);
+                return this.updatePost(postid, "usersdisagreed", disagreed); // either way update usersdisagreed on the post
+            } else return null;
+        } else return null;
     }
 
     async createComment(post, user, date, text, agrees, disagrees) {
