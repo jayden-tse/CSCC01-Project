d87feb6b5fcd33f0d38e060773eb868b5d6b9368
Phillip Pham <phoneqw@gmail.com>
2020-11-09 16:45:54 -0500

    YES-119 made fix to overwriten master change

--- src/routes.js
+++ src/routes.js
@@ -18,13 +18,13 @@ router.use(cors());
 /* USER */
 
 // Check existing username
-router.get('/user/check/username', userController.user_check_username_get);
+router.put('/user/check/username', userController.user_check_username_get);
 
 // Check existing email address
-router.get('/user/check/email', userController.user_check_email_get);
+router.put('/user/check/email', userController.user_check_email_get);
 
 // Check existing phone number
-router.get('/user/check/phonenum', userController.user_check_phonenum_get);
+router.put('/user/check/phonenum', userController.user_check_phonenum_get);
 
 // Update user password
 router.put('/user/update/password', userController.user_update_password_put);
@@ -36,13 +36,22 @@ router.put('/user/update/email', userController.user_update_email_put);
 router.put('/user/update/phonenum', userController.user_update_phonenum_put);
 
 // Send user password recovery email
-router.put('/user/send/password', userController.user_send_new_password_recovery_email);
+router.put(
+  '/user/send/password',
+  userController.user_send_new_password_recovery_email
+);
 
 // Send user new email confirmation email
-router.put('/user/send/email', userController.user_send_new_email_confirmation_email);
+router.put(
+  '/user/send/email',
+  userController.user_send_new_email_confirmation_email
+);
 
 // Send user new phone number sms
-router.put('/user/send/phonenum', userController.user_send_new_phonenum_confirmation_sms);
+router.put(
+  '/user/send/phonenum',
+  userController.user_send_new_phonenum_confirmation_sms
+);
 
 /* SIGNUP */
 
@@ -62,9 +71,6 @@ router.put('/logout', loginController.deauth);
 // Get profile
 router.get('/profile', profileController.profile_get);
 
-// Get other user profile
-router.get('/profile/other', profileController.profile_other_get);
-
 // Get profile picks
 router.get('/profile/picks', profileController.profile_picks_get);
 
@@ -75,19 +81,28 @@ router.get('/profile/tracker', profileController.profile_tracker_get);
 router.get('/profile/picture', profileController.profile_get_picture);
 
 // Update profile picture
-router.put('/profile/update/picture', profileController.profile_update_picture_put);
+router.put(
+  '/profile/update/picture',
+  profileController.profile_update_picture_put
+);
 
 // Update profile about
 router.put('/profile/update/about', profileController.profile_update_about_put);
 
 // Update profile status
-router.put('/profile/update/status', profileController.profile_update_status_put);
+router.put(
+  '/profile/update/status',
+  profileController.profile_update_status_put
+);
 
 // Update profile picks
 router.put('/profile/update/picks', profileController.profile_update_picks_put);
 
 // Update profile tracker
-router.put('/profile/update/tracker', profileController.profile_update_tracker_put);
+router.put(
+  '/profile/update/tracker',
+  profileController.profile_update_tracker_put
+);
 
 // Update profile ACS
 router.put('/profile/update/ACS', profileController.profile_update_ACS_put);
@@ -112,16 +127,28 @@ router.delete('/trivia/delete/question', triviaController.questions_del);
 /* PICKS & PREDICTIONS */
 
 // Create matches
-router.put('/picksandpredictions/create/match', pickspredictionsController.matches_put);
+router.put(
+  '/picksandpredictions/create/match',
+  pickspredictionsController.matches_put
+);
 
 // Get matches
-router.get('/picksandpredictions/match', pickspredictionsController.matches_get);
+router.get(
+  '/picksandpredictions/match',
+  pickspredictionsController.matches_get
+);
 
 // Update matches
-router.put('/picksandpredictions/update/match', pickspredictionsController.matches_update_put);
+router.put(
+  '/picksandpredictions/update/match',
+  pickspredictionsController.matches_update_put
+);
 
 // Delete matches
-router.delete('/picksandpredictions/delete/match', pickspredictionsController.matches_del);
+router.delete(
+  '/picksandpredictions/delete/match',
+  pickspredictionsController.matches_del
+);
 
 /* DEBATE */
 
@@ -129,7 +156,10 @@ router.delete('/picksandpredictions/delete/match', pickspredictionsController.ma
 router.put('/debates/create/topic', debateController.debate_topics_put);
 
 // Create debate submission
-router.put('/debates/create/submission', debateController.debate_submission_put);
+router.put(
+  '/debates/create/submission',
+  debateController.debate_submission_put
+);
 
 // Get debate topics
 router.get('/debates/topic', debateController.debate_topics_get);
@@ -138,13 +168,19 @@ router.get('/debates/topic', debateController.debate_topics_get);
 router.get('/debates/submission', debateController.debate_submission_get);
 
 // Get debate submission time limit
-router.get('/debates/submission/time', debateController.debate_submission_time_limit_get);
+router.get(
+  '/debates/submission/time',
+  debateController.debate_submission_time_limit_get
+);
 
 // Update debate topics
 router.put('/debates/update/topic', debateController.debate_update_topics_put);
 
 // Update debate submission score
-router.put('/debates/submission/update/score', debateController.debate_submission_update_score_put);
+router.put(
+  '/debates/submission/update/score',
+  debateController.debate_submission_update_score_put
+);
 
 // Delete debate topics
 router.delete('/debates/delete/topic', debateController.debate_topics_del);
@@ -164,11 +200,16 @@ router.get('/thezone/post/all', thezoneController.the_zone_all_posts_get);
 router.put('/thezone/update/post', thezoneController.the_zone_update_post_put);
 
 // Update the zone post agree
-router.put('/thezone/update/agree', thezoneController.the_zone_update_agree_put);
+router.put(
+  '/thezone/update/agree',
+  thezoneController.the_zone_update_agree_put
+);
 
 // Update the zone post's comment
-router.put('/thezone/update/post/comment', thezoneController.the_zone_comment_put);
-
+router.put(
+  '/thezone/update/post/comment',
+  thezoneController.the_zone_comment_put
+);
 
 // Delete the zone post
 router.delete('/thezone/delete/post', thezoneController.the_zone_post_del);
